393cab94e73502a1d83055da65153cc1740d0afc
Monika Urban <monika.urban1990@gmail.com>
2017-07-11 01:58:07 +0200

    in progres

---
+++ script.js
@@ -0,0 +1,12 @@
+
+	var i = 0;
+	function liczKlik() {
+		guzik = document.getElementById('guzik');
+		if (i<1000, i++) {
+	guzik.innerHTML = i;
+}
+if (i>=10) {
+	motivation = document.getElementById("motivation");
+	motivation.innerHTML = "Brawo, to już 10!"
+}
+}
